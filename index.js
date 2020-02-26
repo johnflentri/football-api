b6d90e33eb0c7e89ecf64ce300d4f8523a0fbbe5
johnflentri <jpflentri@gmail.com>
2020-02-26 17:44:49 +0100

    POST endpoint is working

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const teamRouter = require("./team/router")
-
 const express = require("express")
-
 const app = express()
-
 const port = process.env.PORT || 4000
-
+const bodyParser = require("body-parser");
+const jsonParser = bodyParser.json()
+app.use(jsonParser)
 app.use(teamRouter)
 
 app.listen(port, () => console.log("listening on port " + port));
