4ae4d06f5395ca77dd6af41032b6050a3806245f
johnflentri <jpflentri@gmail.com>
2020-02-26 16:40:28 +0100

    Database created

---
+++ db.js
@@ -0,0 +1,13 @@
+const Sequelize = require("sequelize")
+
+const databaseUrl = process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres'
+
+const db = new Sequelize(databaseUrl)
+
+db.sync()
+  .then(() => console.log('Database schema has been updated'))
+  .catch(err => {
+    console.error("Something went wrong", err)
+  })
+
+module.exports = db
